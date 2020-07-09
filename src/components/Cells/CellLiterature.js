b344136c36fa7f2a9c0903523ca352b70fdde57f
Javier Ferrer <javier.f.g@um.es>
2020-07-09 11:42:36 +0200

    removed literature icon

--- src/components/Cells/CellLiterature.js
+++ src/components/Cells/CellLiterature.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 
 import CellLink from './CellLink';
 import NaLabel from './NaLabel';
@@ -9,14 +7,11 @@ function CellLiterature({ value, ensemblId }) {
   if (!value) return <NaLabel center />;
 
   return (
-    <>
-      <FontAwesomeIcon icon={faFileAlt} />{' '}
-      <CellLink
-        url="https://www.targetvalidation.org/evidence/$$/MONDO_0100096?view=sec:literature"
-        accession={ensemblId}
-        label={value}
-      />
-    </>
+    <CellLink
+      url="https://www.targetvalidation.org/evidence/$$/MONDO_0100096?view=sec:literature"
+      accession={ensemblId}
+      label={value}
+    />
   );
 }
 
